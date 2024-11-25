a8c481f8608e46ff1bd38af9f69c9367ea950ca8
jackiexiao <707610215@qq.com>
2024-11-25 11:38:23 +0800

    fix tailwindcss warning & update readme & add vscode extension recommandation

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,10 +1,10 @@
-"use client"
+'use client'
 
-import * as React from "react"
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
+import * as React from 'react'
+import { Moon, Sun } from 'lucide-react'
+import { useTheme } from 'next-themes'
 
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button'
 
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme()
@@ -13,7 +13,7 @@ export function ThemeToggle() {
     <Button
       variant="ghost"
       size="icon"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
     >
       <Sun className="size-6 dark:hidden" />
       <Moon className="hidden size-6 dark:block" />
